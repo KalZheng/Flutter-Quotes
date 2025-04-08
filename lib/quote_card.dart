f32b3dbd656ba237cc6284b0e708ccf9aff92e71
kal <lg1443@liguan.com.tw>
2025-04-08 17:30:05 +0800

    add delete button and function

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -4,8 +4,9 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
+  final VoidCallback delete;
 
-  QuoteCard({ required this.quote });
+  QuoteCard({ required this.quote, required this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +31,13 @@ class QuoteCard extends StatelessWidget {
                 fontSize: 14,
                 color: Colors.grey[800],
               ),
+            ),
+            SizedBox(height: 8),
+            TextButton.icon(
+              onPressed: delete,
+              icon: const Icon(Icons.delete),
+              label: const Text('delete quote'),
+              iconAlignment: IconAlignment.start,
             ),
           ],
         ),
