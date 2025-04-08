2b4cafdf797c57f549a6f6a5f37f29fb774a3bcb
kal <lg1443@liguan.com.tw>
2025-04-08 16:08:06 +0800

    change listing display to column objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
@@ -22,6 +24,35 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'kal3', text: 'Holy Molly'),
   ];
 
+  Widget quoteTemplate(quote){
+    return Card(
+        margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 6),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ],
+          ),
+        ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +64,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
